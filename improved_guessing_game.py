3e57eb14125f2eb47d432936e5c4fcfbcd6a6b99
HissingPython <34667241+HissingPython@users.noreply.github.com>
2017-12-20 14:17:26 -0500

    Actual fix - program works

---
+++ improved_guessing_game.py
@@ -0,0 +1,22 @@
+import random
+ 
+ 
+max_number = int(input('I will choose a number between one and whatever maximum you set. Please choose the maximum number now: ' ))
+done = False
+while not done: #This is the loop to keep the game going even after a successful guess. It resets the random number and starts it over
+    n = random.randint(1, max_number)
+    guess = False
+    while guess == False: #This is the loop that will let the player keep guessing if they guess wrong the first time.
+        ans = int(input('Enter your guess - or enter "0" to quit: '))
+        if ans == 0:
+            print('Thank you for playing!')
+            done = True #This breaks the game loop
+            break #This is to break the guessing loop
+        else:
+            if ans == n:
+                print('Success you win! I will now choose another number.')
+                guess = True
+            elif ans > n:
+                print('That is too high. Guess again!')
+            else:
+                print('That is too low. Keep guessing!')
