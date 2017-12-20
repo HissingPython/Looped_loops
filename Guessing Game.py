71fe976b378302e02a21f3d71b98cef145f43d1f
HissingPython <34667241+HissingPython@users.noreply.github.com>
2017-12-19 22:35:21 -0500

    Fixed the other loop and gave it a better name

--- Guessing Game.py
+++ Guessing Game.py
@@ -5,8 +5,8 @@ max_number = int(input('I will choose a number between one and whatever maximum 
 done = False
 while not done: #This is the loop to keep the game going even after a successful guess. It resets the random number and starts it over
     n = random.randint(1, max_number)
-    z = 10
-    while z == 10: #This is the loop that will let the player keep guessing if they guess wrong the first time.
+    guess = incorrect
+    while guess == incorrect: #This is the loop that will let the player keep guessing if they guess wrong the first time.
         ans = int(input('Enter your guess - or enter "0" to quit: '))
         if ans == 0:
             print('Thank you for playing!')
@@ -15,7 +15,7 @@ while not done: #This is the loop to keep the game going even after a successful
         else:
             if ans == n:
                 print('Success you win! I will now choose another number.')
-                z = 11
+                guess = correct
             elif ans > n:
                 print('That is too high. Guess again!')
             else:
